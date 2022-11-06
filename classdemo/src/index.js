d73c3a7562a9fccd91d861c38c3fd83b4bcd92fd
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-11-06 22:02:39 +0000

    playing with dark and light themes

--- classdemo/src/index.js
+++ classdemo/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-//import './sass/App.scss';
+// import './sass/App.scss';
 import App from './App';
 
 const LightTheme = React.lazy(() => import('./components/LightTheme'));
 const DarkTheme = React.lazy(() => import('./components/DarkTheme'));
 
 const ThemeSelector = ({children}) => {
-  const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light");
+  console.log(theme)
+  localStorage.setItem('theme', theme)
   return(
     <React.Suspense fallback={<></>} >
       {theme === 'dark' && <DarkTheme />}
-      {theme === 'light' && <LightTheme />}
+      {!theme === 'light' && <LightTheme />}
+      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}> Change Mode</button>
       {children}
     </React.Suspense>
   )
