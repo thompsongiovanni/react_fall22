169a3a58b3ba5a52f66dd7d661c7275353097c35
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-11-06 19:43:40 +0000

    worked on Header

--- classdemo/src/components/Header.jsx
+++ classdemo/src/components/Header.jsx
@@ -2,20 +2,31 @@ import React from "react";
 
 class Header extends React.Component{
     //constructor for state
-
-    //lifecycle methods
+    constructor(props){
+        super(props)
+        this.state = {
+            count: 0,
+            theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+        }
+    }
 
     render(){
         const { linkText, linkUrl } = this.props
-
+        // this.props.linkText
         return(
-            <header className="App-header">
-                <p>
+            <header className={`App-header ${this.state.theme} ${this.state.count}`}>
+                <p className={"testing-this" + this.state.theme}>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a className="App-link" href={linkUrl} target="_blank" rel="noopener noreferrer" >
                     {linkText}
                 </a>
+                <span>
+                    You have clicked on the button {this.state.count} times!
+                </span>
+                <button onClick={() => this.setState({ count : this.state.count + 1})}>
+                    Increment Count
+                </button>
           </header>
         )
     }
---
+++ classdemo/src/components/MainBody.jsx
@@ -0,0 +1,1 @@
+// create a functional component with hooks
