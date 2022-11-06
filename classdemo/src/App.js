f2d0710743cb5052ba179dfc93322655e9a29b93
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-11-06 23:25:23 +0000

    modified buttons for theme switch

--- classdemo/src/App.js
+++ classdemo/src/App.js
@@ -1,9 +1,28 @@
 import Header from "./components/Header";
-
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+      <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Button variant="danger" onClick={() => {localStorage.setItem('theme', localStorage.getItem('theme') === 'dark' ? 'light' : 'dark');
+            window.location.reload()
+          }}> 
+              Change Mode
+          </Button>
+        </Nav.Item>
+      </Nav>
       <Header 
         linkText="Learn React"
         linkUrl="https://reactjs.org"
--- classdemo/src/index.js
+++ classdemo/src/index.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './sass/App.scss';
+import Spinner from 'react-bootstrap/Spinner';
 import App from './App';
 
 const LightTheme = React.lazy(() => import('./components/LightTheme'));
 const DarkTheme = React.lazy(() => import('./components/DarkTheme'));
 
 const ThemeSelector = ({children}) => {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light");
-  console.log(theme)
+  const theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light";
   localStorage.setItem('theme', theme)
   return(
-    <React.Suspense fallback={<></>} >
+    <React.Suspense
+      fallback={<Spinner animation="border" variant="primary"/>} 
+      key={theme}
+    >
       {theme === 'dark' && <DarkTheme />}
-      {!theme === 'light' && <LightTheme />}
-      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}> Change Mode</button>
+      {theme === 'light' && <LightTheme />}
+      {/*<Button variant="danger">Danger</Button> */}
+      
       {children}
     </React.Suspense>
   )
